34783ea3a08bf7dbb153214d029533f5e81c6cc8
charles66820 <charles.goedefroit@gmail.com>
2017-11-08 18:58:32 +0100

    le site!!!! (enfin! mdr)

---
+++ assets/js/champion.js
@@ -0,0 +1,129 @@
+//variable
+var load = "assets/php/champion.php?";
+var limit = 10;
+
+//premier chargement
+$("#table").load("assets/php/champion.php");
+
+//quant on clique sur la colonne nom
+$("#nom").click(function() {
+  if ($(this).children().text() == ">") {
+    $(".fel").html("&nbsp");
+    $(this).children().text("<");
+    $("#table").load("assets/php/champion.php?colonne=nom");
+    load = "assets/php/champion.php?colonne=nom";
+    limit = 10;
+  }else if ($(this).children().text() == "<") {
+    $(".fel").html("&nbsp");
+    $(this).children().text(">");
+    $("#table").load("assets/php/champion.php?colonne=nom&ordre=0");
+    load = "assets/php/champion.php?colonne=nom&ordre=0";
+    limit = 10;
+  }else{
+    $(".fel").html("&nbsp");
+    $(this).children().text("<");
+    $("#table").load("assets/php/champion.php?colonne=nom");
+    load = "assets/php/champion.php?colonne=nom";
+    limit = 10;
+  }
+})
+
+//quant on clique sur la colonne nationalité
+$("#nat").click(function() {
+  if ($(this).children().text() == ">") {
+    $(".fel").html("&nbsp");
+    $(this).children().text("<");
+    $("#table").load("assets/php/champion.php?colonne=nationaliter");
+    load = "assets/php/champion.php?colonne=nationaliter";
+    limit = 10;
+  }else if ($(this).children().text() == "<") {
+    $(".fel").html("&nbsp");
+    $(this).children().text(">");
+    $("#table").load("assets/php/champion.php?colonne=nationaliter&ordre=0");
+    load = "assets/php/champion.php?colonne=nationaliter&ordre=0";
+    limit = 10;
+  }else{
+    $(".fel").html("&nbsp");
+    $(this).children().text("<");
+    $("#table").load("assets/php/champion.php?colonne=nationaliter");
+    load = "assets/php/champion.php?colonne=nationaliter";
+    limit = 10;
+  }
+})
+
+//quant on clique sur la colonne titre
+$("#titre").click(function() {
+  if ($(this).children().text() == ">") {
+    $(".fel").html("&nbsp");
+    $(this).children().text("<");
+    $("#table").load("assets/php/champion.php?colonne=titre");
+    load = "assets/php/champion.php?colonne=titre";
+    limit = 10;
+  }else if ($(this).children().text() == "<") {
+    $(".fel").html("&nbsp");
+    $(this).children().text(">");
+    $("#table").load("assets/php/champion.php?colonne=titre&ordre=0");
+    load = "assets/php/champion.php?colonne=titre&ordre=0";
+    limit = 10;
+  }else{
+    $(".fel").html("&nbsp");
+    $(this).children().text("<");
+    $("#table").load("assets/php/champion.php?colonne=titre");
+    load = "assets/php/champion.php?colonne=titre";
+    limit = 10;
+  }
+})
+
+//quant on clique sur la colonne édition
+$("#edition").click(function() {
+  if ($(this).children().text() == ">") {
+    $(".fel").html("&nbsp");
+    $(this).children().text("<");
+    $("#table").load("assets/php/champion.php?colonne=edition");
+    load = "assets/php/champion.php?colonne=edition";
+    limit = 10;
+  }else if ($(this).children().text() == "<") {
+    $(".fel").html("&nbsp");
+    $(this).children().text(">");
+    $("#table").load("assets/php/champion.php?colonne=edition&ordre=0");
+    load = "assets/php/champion.php?colonne=edition&ordre=0";
+    limit = 10;
+  }else{
+    $(".fel").html("&nbsp");
+    $(this).children().text("<");
+    $("#table").load("assets/php/champion.php?colonne=edition");
+    load = "assets/php/champion.php?colonne=edition";
+    limit = 10;
+  }
+})
+
+//quant on clique sur la colonne type
+$("#type").click(function() {
+  if ($(this).children().text() == ">") {
+    $(".fel").html("&nbsp");
+    $(this).children().text("<");
+    $("#table").load("assets/php/champion.php?colonne=type");
+    load = "assets/php/champion.php?colonne=type";
+    limit = 10;
+  }else if ($(this).children().text() == "<") {
+    $(".fel").html("&nbsp");
+    $(this).children().text(">");
+    $("#table").load("assets/php/champion.php?colonne=type&ordre=0");
+    load = "assets/php/champion.php?colonne=type&ordre=0";
+    limit = 10;
+  }else{
+    $(".fel").html("&nbsp");
+    $(this).children().text("<");
+    $("#table").load("assets/php/champion.php?colonne=type");
+    load = "assets/php/champion.php?colonne=type";
+    limit = 10;
+  }
+})
+
+//quant on clique sur voire plus
+$("#load").click(function() {
+  $.get( load+'&limit='+limit, function(data) {
+    $("#table").append(data);
+    limit+=10;
+  });
+})
---
+++ assets/php/champion.php
@@ -0,0 +1,51 @@
+<?php
+// connection a la base d donnée
+$conn = mysqli_connect('localhost', 'root', '', 'webdata');
+
+if (!$conn){
+  die("connection à la db a échouer : ".mysqli_connect_error());
+}else {
+
+  //variable
+  if (isset($_GET["limit"])) {
+    $limit = $_GET["limit"];//integer
+  }else {
+    $limit = 0;
+  }
+  if (isset($_GET["colonne"])){
+    $colonne = $_GET["colonne"];//string
+  }else {
+    $sql = "SELECT * FROM champions LIMIT 10 OFFSET $limit";
+  }
+  if (isset($_GET["ordre"])) {
+    $ordre = $_GET["ordre"];//boolean
+  }else {
+    $ordre = 1;
+  }
+
+  //ordre
+  if (isset($_GET["colonne"])) {
+    if ($colonne == "edition") {
+      if ($ordre) {
+        $sql = "SELECT * FROM champions ORDER BY SUBSTR(edition, -4) ASC LIMIT 10 OFFSET $limit";
+      }else {
+        $sql = "SELECT * FROM champions ORDER BY SUBSTR(edition, -4) DESC LIMIT 10 OFFSET $limit";
+      }
+    }else {
+      if ($ordre) {
+        $sql = "SELECT * FROM champions ORDER BY $colonne ASC LIMIT 10 OFFSET $limit";
+      }else {
+        $sql = "SELECT * FROM champions ORDER BY $colonne DESC LIMIT 10 OFFSET $limit";
+      }
+    }
+  }
+
+  //sql
+  $result = mysqli_query($conn, $sql);
+  while ($row = mysqli_fetch_assoc($result)){
+    echo '<tr>
+    <td>'.$row["nom"].'</td><td>'.$row["nationaliter"].'</td><td>'.$row["titre"].'</td><td>'.$row["edition"].'</td><td>'.$row["type"].'</td>
+    </tr>';
+  }
+}
+?>
